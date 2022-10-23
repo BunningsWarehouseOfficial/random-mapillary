66fea58009a9b6222ecf554389a1a192c8c6c4d4
Kristian <kristianrados40@gmail.com>
2022-10-23 22:08:39 +0800

    feat: new manual filtering script options

--- manual_filter.py
+++ manual_filter.py
@@ -18,6 +18,12 @@ from tkinter import *
 import cv2 as cv
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--other-only", action="store_true", help="Only allow the OTHER and Skip actions.")
+parser.add_argument("-c", "--copy", action="store_true", help="Copy images instead of moving them.")
+args = parser.parse_args()
+
+
 def ResizeWithAspectRatio(image, width=None, height=None, inter=cv.INTER_AREA):
     dim = None
     (h, w) = image.shape[:2]
@@ -36,10 +42,16 @@ def main():
     root = Tk()
     root.withdraw()
     indir = filedialog.askdirectory(title="Input directory of UNFILTERED images")
-    passdir = filedialog.askdirectory(title="Directory to move images that PASSED the filter")
-    faildir = filedialog.askdirectory(title="Directory to move images that FAILED the filter")
-    recheckdir = filedialog.askdirectory(title="Directory to move images that need to be RECHECKED")
+    if not args.other_only:
+        passdir = filedialog.askdirectory(title="Directory to move images that PASSED the filter")
+        faildir = filedialog.askdirectory(title="Directory to move images that FAILED the filter")
+        recheckdir = filedialog.askdirectory(title="Directory to move images that need to be RECHECKED")
     otherdir = filedialog.askdirectory(title="Directory to move images that fulfill some OTHER criteria")
+
+    if args.copy:
+        fn = shutil.copy
+    else:
+        fn = shutil.move
 
     imgs = glob(f"{indir}/*.jpg")
     for ii, img_path in enumerate(imgs):
@@ -51,17 +63,17 @@ def main():
         cv.imshow(img_path, img)
         cv.moveWindow(img_path, 0, 0)
         pressedKey = cv.waitKey(0) & 0xFF
-        if pressedKey == ord('p') or pressedKey == ord('P'):
-            shutil.move(img_path, os.path.join(passdir, os.path.basename(img_path)))
+        if (pressedKey == ord('p') or pressedKey == ord('P')) and not args.other_only:
+            fn(img_path, os.path.join(passdir, os.path.basename(img_path)))
             print(f"[{ii + 1}] Marked as PASS: {os.path.basename(img_path)}")
-        elif pressedKey == ord('f') or pressedKey == ord('F'):
-            shutil.move(img_path, os.path.join(faildir, os.path.basename(img_path)))
+        elif (pressedKey == ord('f') or pressedKey == ord('F')) and not args.other_only:
+            fn(img_path, os.path.join(faildir, os.path.basename(img_path)))
             print(f"[{ii + 1}] Marked as FAIL: {os.path.basename(img_path)}")
-        elif pressedKey == ord('r') or pressedKey == ord('R'):
-            shutil.move(img_path, os.path.join(recheckdir, os.path.basename(img_path)))
+        elif (pressedKey == ord('r') or pressedKey == ord('R')) and not args.other_only:
+            fn(img_path, os.path.join(recheckdir, os.path.basename(img_path)))
             print(f"[{ii + 1}] Marked as RECHECK: {os.path.basename(img_path)}")
         elif pressedKey == ord('o') or pressedKey == ord('O'):
-            shutil.move(img_path, os.path.join(otherdir, os.path.basename(img_path)))
+            fn(img_path, os.path.join(otherdir, os.path.basename(img_path)))
             print(f"[{ii + 1}] Marked as OTHER: {os.path.basename(img_path)}")
         # Helpful here: https://www.asciitable.com
         elif pressedKey == 8 or pressedKey == 127:  # Backspace or Delete keys
